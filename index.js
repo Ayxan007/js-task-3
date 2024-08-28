278e8b6bb1a6b3e5bbc05a0b7b1778cfd9047e27
AyxanR <ahuseynli630@gmail.com>
2024-08-28 11:13:03 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+1.// Array.prototype.max = function() {
+//     return Math.max.apply(null, this);
+//   };
+  
+2.//   Array.prototype.min = function() {
+//     return Math.min.apply(null, this);
+//   };
+  
+//   let numbers = [35,2,65,7,8,9,12,121,33,99];
+  
+//   console.log(`Max value is: ${numbers.max()}` +
+//     `\nMin value is: ${numbers.min()}`);
+
+
+ 3.   // function indexMax() {
+    //     let arrayEdificiosNiveis = [22,30,50];
+    //     var indexMenor = arrayEdificiosNiveis.indexOf(Math.max(arrayEdificiosNiveis));
+    //     console.log(indexMenor);
+    //   }
+    //   indexMax();
+
+ 4.   //   function indexMin() {
+    //     let arrayEdificiosNiveis = [14,12,17];
+    //     var indexMenor = arrayEdificiosNiveis.indexOf(Math.min(arrayEdificiosNiveis));
+    //     console.log(indexMenor);
+    //   }
+    //   indexMin();
+
+ 5.   //  function sum_even_index(x) {
+    //     let sum = 0
+    //     for (let i in x) {
+    //       if (x.indexOf[i] % 2 === 0) {
+    //         sum = sum + x[i];
+    //       }
+      
+    //     }
+    //     return sum;
+    //   }
+      
+    //   console.log(sum_even_index([1, 2, 3, 4]))
+6. 
+
+
+    
